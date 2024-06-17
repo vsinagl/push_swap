28e38ebc19152c5378cc2126f25129b1ac7d02d9
ViktorSinagl <viktor.sinagl@gmail.com>
2024-06-17 15:04:50 +0200

    work on checker..

--- src/bonus/checker.c
+++ src/bonus/checker.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsinagl <vsinagl@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 09:12:15 by vsinagl           #+#    #+#             */
+/*   Updated: 2024/06/13 17:17:31 by vsinagl          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "checker.h"
 #include "get_next_line_bonus.h"
@@ -66,6 +77,21 @@ int	*process_input(int argc, char *argv[], int option)
 	return (arr);
 }
 
+//1 == stack ok, 0 = stack ko
+int	is_stack_sorted(t_stack *stack)
+{
+	unsigned int		j;
+
+	j = 0;
+	while(j < (stack->len - 1))
+	{
+		if (get_stack_value(stack, j) > get_stack_value(stack, (j + 1)))
+			return 0;
+		j++;
+	}
+	return 1;
+}
+
 int	check_inputs(int *input, int len)
 {
 	char *instruction;
@@ -74,32 +100,38 @@ int	check_inputs(int *input, int len)
 
 	stacka = stack_init(input, len, 'a');
 	stackb = stack_init(NULL, 0, 'b');
-	instruction = "";
+	instruction = get_next_line(STDIN_FILENO);
 	while(instruction != NULL)
 	{
 		//zde napsat vlastni str_cmp  a pridat do knihovny!!!
-		if (strcmp(instruction, "sa\n"))
+		printf(">%s", instruction);
+		if (strcmp(instruction, "sa\n") == 0)
 			swap(1,stacka);
-		else if (strcmp(instruction, "sb\n"))
+		else if (strcmp(instruction, "sb\n") == 0)
 			swap(1, stackb);
-		else if (strcmp(instruction, "ra\n"))
+		else if (strcmp(instruction, "ss\n") == 0)
+			swap(2, stackb, stacka);
+		else if (strcmp(instruction, "ra\n") == 0)
 			rotate(1, stacka);
-		else if (strcmp(instruction, "rb\n"))
+		else if (strcmp(instruction, "rb\n") == 0)
 			rotate(1, stackb);
-		else if (strcmp(instruction, "rra\n"))
+		else if (strcmp(instruction, "rr\n") == 0)
+			rotate(2, stackb, stacka);
+		else if (strcmp(instruction, "rra\n") == 0)
 			rev_rotate(1, stacka);
-		else if (strcmp(instruction, "rrb\n"))
+		else if (strcmp(instruction, "rrb\n") == 0)
 			rev_rotate(1, stackb);
-		else if (strcmp(instruction, "pa\n"))
-			push(stacka, stackb);
-		else if (strcmp(instruction, "pb\n"))
+		else if (strcmp(instruction, "rrr\n") == 0)
+			rev_rotate(2, stackb, stacka);
+		else if (strcmp(instruction, "pa\n") == 0)
 			push(stackb, stacka);
+		else if (strcmp(instruction, "pb\n") == 0)
+			push(stacka, stackb);
+		else
+			return 0;
+		instruction = get_next_line(STDIN_FILENO);
 	}
-	ft_printf("sorted stack?\n");
-	print_stack(stacka);
-	print_stack(stackb);
-	return (1);
-
+	return (is_stack_sorted(stacka));
 }
 
 
@@ -109,17 +141,21 @@ int	main(int argc, char **argv)
 
 	input = NULL;
 	if (argc <= 1)
+	{
 		return (1);
+	}
 	else if (argc == 2)
 	{
-		argv = ft_split(argv[1], ' ' );
+		argv = ft_split(argv[1], ' ');
 		argc = 1;
 		while (argv[argc - 1] != NULL)
 			argc++;
 		input = process_input(argc, argv, 1);
 	}
 	else if (argc > 2)
+	{
 		input = process_input(argc, argv, 0);
+	}
 	if (check_inputs(input, (argc - 1)) == 0)
 		ft_printf(RED "KO" RESET);
 	else
